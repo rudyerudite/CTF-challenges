205ca4382568154949c3c0865691629859bb4e45
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-05-11 22:33:36 +0530

    Update tes.py

--- tes.py
+++ tes.py
@@ -9,8 +9,7 @@ def decrypt(msg):
 	for i in range(len(msg)-1):
 		if((i+1)%3!=0):
 			v+=msg[i]
-	#if("asis" in long_to_bytes(int(v,2))):
-	print("Hagrid's right!!!")
+	print("Check this one!!!")
 	print(long_to_bytes(int(v,2)))
 	return v
 
@@ -22,33 +21,4 @@ with open("FLAG.enc") as f:
 		
 for i in range(100):
 	dec=decrypt(dec)
-
-#for i in range(100):
-	#dec=decrypt(dec)
-'''def encrypt(msg):
-	enc = [msg[i:i+2] + str(int(msg[i]) ^ int(msg[min(i+1, len(msg)-1)])) for i in range(0, len(msg), 2)]
-	enc="".join(enc)
-	print(enc)
-	return enc
-
-def decrytp(msg):
-	d=""
-	for i in range(len(msg)-1):
-		if((i+1)%3!=0):
-			d+=msg[i]
-	return d
-
-for j in range(2):
-	enc=encrypt(enc)
-
-for j in range(2):
-	enc=decrytp(enc)
-print(enc)
-print(msg)
-if(enc==msg):
-	print("Hagrid's right")
-else:
-	print("Slytherin's upto smth")
-'''
-
 
